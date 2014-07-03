c1f827f625efa20ef962271e82587fc8d98f5c75
TingPing <tingping@tingping.se>
2014-07-03 13:34:44 -0400

    Add gnome-themes-standard

---
+++ gnome-themes-standard.rb
@@ -0,0 +1,21 @@
+require 'formula'
+
+class GnomeThemesStandard < Formula
+  homepage 'http://gnome.org'
+  url 'https://download.gnome.org/core/3.12/3.12.2/sources/gnome-themes-standard-3.12.0.tar.xz'
+  sha256 'a05d1b7ca872b944a69d0c0cc2369408ece32ff4355e37f8594a1b70d13c3217'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'intltool' => :build
+  depends_on 'glib' => :build
+  depends_on 'gettext'
+  depends_on 'TingPing/gnome/gtk+3'
+  depends_on 'TingPing/gnome/librsvg' => ['with-gtk3', :build]
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+
+    system "make install"
+  end
+end
---
+++ librsvg.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Librsvg < Formula
+  homepage 'https://live.gnome.org/LibRsvg'
+  url 'http://ftp.gnome.org/pub/GNOME/sources/librsvg/2.36/librsvg-2.36.3.tar.xz'
+  sha256 '3d7d583271030e21acacc60cb6b81ee305713c9da5e98429cbd609312aea3632'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'TingPing/gnome/gtk+' => :optional
+  depends_on 'TingPing/gnome/gtk+3' => :optional
+  depends_on 'libcroco'
+  depends_on 'libgsf' => :optional
+
+  def install
+    args = ["--disable-dependency-tracking",
+            "--prefix=#{prefix}",
+            "--disable-Bsymbolic",
+            "--enable-tools=yes",
+            "--enable-pixbuf-loader=yes",
+            "--enable-introspection=no"]
+
+    args << "--enable-svgz" if build.with? 'libgsf'
+
+    system "./configure", *args
+    system "make install"
+  end
+end
