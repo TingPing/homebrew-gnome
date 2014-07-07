1d9e1c1207ae0b56567aa37224b1d09376a44f28
TingPing <tingping@tingping.se>
2014-07-07 17:32:43 -0400

    Fix building gnome-themes-standard

--- gnome-themes-standard.rb
+++ gnome-themes-standard.rb
@@ -9,13 +9,34 @@ class GnomeThemesStandard < Formula
   depends_on 'intltool' => :build
   depends_on 'glib' => :build
   depends_on 'gettext'
-  depends_on 'TingPing/gnome/gtk+3'
-  depends_on 'TingPing/gnome/librsvg' => ['with-gtk+3', :build]
+  depends_on 'TingPing/gnome/gtk+3' => :recommended
+  depends_on 'TingPing/gnome/gtk+' => :optional
+  depends_on 'TingPing/gnome/librsvg' => ['with-gtk+3', :build] # FIXME: gtk+ if build.with gtk+
+
+  if build.without?("gtk+3") && build.without?("gtk+")
+    odie "You must build with gtk+ and/or gtk+3 support."
+  end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+    ]
 
-    system "make install"
+    if build.without? 'gtk+3'
+      args << '--disable-gtk3-engine'
+    elsif build.without? 'gtk+'
+      args << '--disable-gtk2-engine'
+      ENV["GTK_UPDATE_ICON_CACHE"] = "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache"
+    end
+
+    system "./configure", *args
+
+    # Install requires the svg module for gdk-pixbuf
+    ENV["GDK_PIXBUF_MODULEDIR"] = "#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
+    ENV["GDK_PIXBUF_MODULE_FILE"] = "#{HOMEBREW_PREFIX}/etc/gtk-2.0/gdk-pixbuf.loaders"
+
+    system "#{Formula["gdk-pixbuf"].opt_bin}/gdk-pixbuf-query-loaders", "--update-cache"
+    system "make", "install"
   end
 end
