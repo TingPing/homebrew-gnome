29f93bbe8eeb0815bc076dc2c6653c36c5ce0494
TingPing <TingPing@users.noreply.github.com>
2014-07-29 05:19:04 -0400

    Update gtk+3.rb

--- gtk+3.rb
+++ gtk+3.rb
@@ -20,7 +20,7 @@ class Gtkx3 < Formula
   depends_on 'gdk-pixbuf'
   depends_on 'atk'
   depends_on 'at-spi2-atk'
-  depends_on 'pango' => 'without-x'
+  depends_on 'pango' => 'without-x11'
   depends_on 'cairo' => 'without-x11'
 
   def install
