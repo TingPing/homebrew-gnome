fe3bded579a1ee7497e07966c8aed5a96a7578b5
TingPing <tingping@tingping.se>
2014-08-01 03:08:25 -0400

    Fix building gnome-icon-theme w/o x11

--- gnome-icon-theme.rb
+++ gnome-icon-theme.rb
@@ -7,7 +7,7 @@ class GnomeIconTheme < Formula
 
   depends_on "pkg-config" => :build
   depends_on "gettext" => :build
-  depends_on "gtk+3" => :build # for gtk3-update-icon-cache
+  depends_on "TingPing/gnome/gtk+3" => :build # for gtk3-update-icon-cache
   depends_on "icon-naming-utils" => :build
   depends_on "intltool" => :build
 
