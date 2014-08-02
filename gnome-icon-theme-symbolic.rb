2ea1682fa7cab7d69db5bcac90fe24b277da9984
TingPing <tingping@tingping.se>
2014-08-02 07:44:22 -0400

    Use gtk+3 from repo

--- gnome-icon-theme-symbolic.rb
+++ gnome-icon-theme-symbolic.rb
@@ -7,10 +7,10 @@ class GnomeIconThemeSymbolic < Formula
 
   depends_on "pkg-config" => :build
   depends_on "gettext" => :build
-  depends_on "gtk+3" => :build # for gtk3-update-icon-cache
+  depends_on "TingPing/gnome/gtk+3" => :build # for gtk3-update-icon-cache
   depends_on "icon-naming-utils" => :build
   depends_on "intltool" => :build
-  depends_on "gnome-icon-theme"
+  depends_on "TingPing/gnome/gnome-icon-theme"
 
   def install
     ENV["GTK_UPDATE_ICON_CACHE"] = "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache"
